23efb81978241fae3b4bb1732255e9a0ac204638
Leticia Ariela <leticiaariela@gmail.com>
2022-01-03 22:43:31 +0000

    Magnesium benefits messages

--- mixed messages.js
+++ mixed messages.js
@@ -1,3 +1,7 @@
+const vitaminA = ['Vitamin A protects your eyes from age-related decline', 'Vitamin A may lower your risk of certain cancers', 'Vitamin A supports a healthy immune system', 'Vitamin A reduces your risk of acne', 'Vitamin A supports bone health', 'Vitamin A promotes healthy growth and reproduction']
+
 const vitaminC = ['Vitamin C may reduce your risk of chronic disease', 'Vitamin C may help manage high blood pressure', 'Vitamin C may lower your risk of heart disease', 'Vitamin C helps prevent iron deficiency', 'Vitamin C boosts immunity', 'Vitamin C protects your memory and thinking as you age'];
 
 const complexB = ['Complex B maintains cell health', 'Complex B essential for the growth of red blood cells', 'Complex B boosts energy levels', 'Complex B helps eyesight health', 'Complex B helps good digestion', 'Complex B is important for hormones and cholesterol production'];
+
+const magnesium = ['Magnesium boosts exercise performance', 'Magnesium fights depression', 'Magnesium fights type 2 diabetes', 'Magnesium can lower blood pressure', 'Magnesium has anti-inflammatory benefits', 'Magnesium helps prevent migraines', 'Magnesium reduces insulin resistance'];
