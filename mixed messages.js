3207210325e1b96f2a15ffbc507afd34bd71672d
Leticia Ariela <leticiaariela@gmail.com>
2022-01-04 21:43:23 +0000

    organizing messages by 5

--- mixed messages.js
+++ mixed messages.js
@@ -1,17 +1,19 @@
 const vitaminsMinerals = ['a', 'c', 'b', 'magnesium', 'manganese', 'selenium'];
 
-const vitaminA = ['Vitamin A protects your eyes from age-related decline', 'Vitamin A may lower your risk of certain cancers', 'Vitamin A supports a healthy immune system', 'Vitamin A reduces your risk of acne', 'Vitamin A supports bone health', 'Vitamin A promotes healthy growth and reproduction']
+const vitaminA = ['Vitamin A protects your eyes from age-related decline', 'Vitamin A may lower your risk of certain cancers', 'Vitamin A reduces your risk of acne', 'Vitamin A supports bone health', 'Vitamin A promotes healthy growth and reproduction']
 
-const vitaminC = ['Vitamin C may reduce your risk of chronic disease', 'Vitamin C may help manage high blood pressure', 'Vitamin C may lower your risk of heart disease', 'Vitamin C helps prevent iron deficiency', 'Vitamin C boosts immunity', 'Vitamin C protects your memory and thinking as you age'];
+const vitaminC = ['Vitamin C may help manage high blood pressure', 'Vitamin C may lower your risk of heart disease', 'Vitamin C helps prevent iron deficiency', 'Vitamin C boosts immunity', 'Vitamin C protects your memory and thinking as you age'];
 
-const complexB = ['Complex B maintains cell health', 'Complex B essential for the growth of red blood cells', 'Complex B boosts energy levels', 'Complex B helps eyesight health', 'Complex B helps good digestion', 'Complex B is important for hormones and cholesterol production'];
+const complexB = ['Complex B maintains cell health', 'Complex B essential for the growth of red blood cells', 'Complex B boosts energy levels', 'Complex B helps eyesight health', 'Complex B is important for hormones and cholesterol production'];
 
-const magnesium = ['Magnesium boosts exercise performance', 'Magnesium fights depression', 'Magnesium fights type 2 diabetes', 'Magnesium can lower blood pressure', 'Magnesium has anti-inflammatory benefits', 'Magnesium helps prevent migraines', 'Magnesium reduces insulin resistance'];
+const magnesium = ['Magnesium boosts exercise performance', 'Magnesium fights depression', 'Magnesium fights type 2 diabetes', 'Magnesium helps prevent migraines', 'Magnesium reduces insulin resistance'];
 
-const manganese = ['Manganese improves bone health', 'Manganese helps reduce inflammation', 'Manganese plays a role in blood sugar regulation', 'Manganese plays a role in the metabolism od nutrients', 'Manganese improves brain function', 'Manganese contributes to good thyroid health', 'Manganese may aid wound healing by playing a role in collagen production'];
+const manganese = ['Manganese improves bone health', 'Manganese plays a role in blood sugar regulation', 'Manganese improves brain function', 'Manganese contributes to good thyroid health', 'Manganese may aid wound healing by playing a role in collagen production'];
 
 const selenium = ['Selenium acts as a powerful antioxidant', 'Selenium helps prevent mental decline', 'Selenium may reduce your risk of certain cancers', 'Selenium boosts immune system', 'Selenium may help reduce asthma symptoms'];
 
 const randomMessage = () => {
+    const randomvitMin = Math.floor(Math.random() * 6)
+    const randomMessage = Math.floor(Math.random( *))
 
 }
